681f08467e544a66e9f1b259b01b1253b70eda1a
Onke Nyathela <onkenyathela7@gmail.com>
2022-01-14 15:17:51 +0200

    1st commit

---
+++ script.js
@@ -0,0 +1,46 @@
+$(document).ready(function()
+{
+    $(window).scroll(function()
+    {
+        if(this.scrollY > 20)
+        {
+            $('.navbar').addClass("sticky");
+        }
+        else
+        {
+            $('.navbar').removeClass("sticky");
+        }
+    }
+
+    );
+    $('.menu-btn').click(function()
+    {
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    }
+    );
+}
+);
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
